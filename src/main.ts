9298040912c49826fa251935fef7dd23212b3e73
JoseAlonzo <josealonzohp@gmail.com>
2025-11-16 22:09:48 -0500

    Mensaje del commit

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,21 @@ async function bootstrap() {
   //prefijo global para la version de la API
   app.setGlobalPrefix('api/v1');
 
+  
+  //configuracion de cors para permitir solicitudes desde el frontend
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  });
+
   //configuracion global de pipes para validaciones
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true,
-}));
+  }));
 
+  //puerto de escucha del servidor
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
