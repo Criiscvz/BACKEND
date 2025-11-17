35224dabc594e2afec9415f51d17d25fc5e2e718
MiguelBarragan <estsjmabs20049a@gmail.com>
2025-11-17 16:54:49 -0500

    Ruta protegida

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  });
 
   //prefijo global para la version de la API
   app.setGlobalPrefix('api/v1');
